86bb0a97dda7c66de1f8745a9f4125e912340a46
Ino Van Winckel <inovanwinckel@hotmail.com>
2016-12-07 19:56:41 +0100

    UltraFinal Mod

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -9,9 +9,11 @@ class RecordController extends Controller
 {
     public function find(Request $request)
     {
+        $file = $request->files->get('file');
+
         $musicFinder = new Musicfinder();
 
-        $result = json_decode($musicFinder->find($request->files->get('file')));
+        $result = json_decode($musicFinder->find($file));
 
         if ($result->status->msg == "Success") {
             return response()->json(['data' => 'everything ok'], 200);
