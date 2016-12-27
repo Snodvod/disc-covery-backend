eb10f1e02d26fa7b1dd6bd44c9ce837b9f45ce2c
Ino Van Winckel <inovanwinckel@hotmail.com>
2016-12-27 17:40:49 +0100

    remove user passwords from seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,27 +15,13 @@ class UsersTableSeeder extends Seeder
 				[
 					'firstname'	=> 'Ino',
 					'lastname'	=> 'Van Winckel',
-					'email'		=> 'admin@admin.com',
-					'password'	=> bcrypt('secret'),
+					'email'		=> 'admin@admin.com'
 				],
 				[
 					'firstname'	=> 'Tim',
 					'lastname'	=> 'Van Dyck',
-					'email'		=> 'user1@user.com',
-					'password'	=> bcrypt('secret'),
-				],
-				[
-					'firstname'	=> 'User',
-					'lastname'	=> '2',
-					'email'		=> 'user2@user.com',
-					'password'	=> bcrypt('user2222'),
-				],
-				[
-					'firstname'	=> 'User',
-					'lastname'	=> '3',
-					'email'		=> 'user3@user.com',
-					'password'	=> bcrypt('user333'),
-				],
+					'email'		=> 'user1@user.com'
+				]
 		]);
 	}
 }
