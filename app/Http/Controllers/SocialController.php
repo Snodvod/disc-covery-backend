96bd244973d9c5af97d56561f201d350f1be60ac
Ino Van Winckel <inovanwinckel@hotmail.com>
2017-01-08 10:57:47 +0100

    Removed crap, ionic push plugin added

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -24,17 +24,6 @@ class SocialController
     }
 
     public function openFacebookDialog() {
-        $response = new StreamedResponse();
-        $response->headers->set('Content-Type', 'text/event-stream');
-        $response->headers->set('Cache-Control', 'no-cache');
 
-        $response->setCallback(
-            function() {
-                echo "event: facebook"; // no retry would default to 3 seconds.
-                echo "data: Dialog open";
-                ob_flush();
-                flush();
-            });
-        $response->send();
     }
 }
