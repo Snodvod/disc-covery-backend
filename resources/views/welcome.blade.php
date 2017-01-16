2924a5094d26d4a4e47aa65725e7cda56fb29fad
Ino Van Winckel <inovanwinckel@hotmail.com>
2017-01-16 17:08:45 +0100

    Responsive design completed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:image" content="/img/logo.svg" />
+    <meta property="og:image" content="/img/logo.svg"/>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -29,7 +29,7 @@
 <div class="container" id="fullpage">
     <div class="row fp-auto-height-responsive section homepage-hero-module" data-anchor="slide1">
         <div class="hero-unit">
-            <h1>For the Record</h1>
+            <h1 class="fatboy">For the Record</h1>
             <h3>The social record player</h3>
             <p>
                 Enjoy the warm sound of vinyl along with the perks of sharing your music with friends
@@ -39,14 +39,13 @@
             </a>
         </div>
         <div class="video-container">
-            <video id="vi" autoplay loop class="fillWidth">
-                <source src="vids/vibes.mp4" type="video/mp4"/>
-                Your browser does not support the video tag. I suggest you upgrade your browser.
-                <source src="vids/vibes.webm" type="video/webm"/>
+            <div class="video-overlay"></div>
+            <video id="vid" loop class="desktop-only fillWidth">
+                <source src="vids/vid.mp4" type="video/mp4"/>
                 Your browser does not support the video tag. I suggest you upgrade your browser.
             </video>
-            <div class="poster hidden">
-                <img src="vids/vibes.jpg" alt="header video snapshot">
+            <div class="poster mobile-only">
+                <div class="image"></div>
             </div>
         </div>
 
@@ -55,48 +54,44 @@
         <div class="col-md-6 col-md-offset-3">
             <div class="row part">
                 <div class="col-md-6">
-                    <h3>What is For the Record?</h3>
+                    <h3>What is <span class="fatboy">For the Record?</span></h3>
                     <p>
                         For the Record is a project which endeavours to bring together the warm, full sound of vinyl
                         records
                         with the pleasure of sharing your music on social platforms like Spotify, Twitter and Facebook.
                     </p>
                 </div>
-                <div class="col-md-6">
+                <div class="desktop-only col-md-6">
                     <div class="logo"></div>
                 </div>
             </div>
             <div class="row part">
-                <div class="col-md-6">
+                <div class="col-md-6 col-xs-4">
                     <a class="" href="/download/android">
                         <div class="android">
                         </div>
                     </a>
                 </div>
-                <div class="col-md-6">
-                    <h3>Download the companion app and start listening!</h3>
-                    <p>
+                <div class="col-md-6 col-xs-8">
+                    <h3 class="companion">Download the companion app and start listening!</h3>
+                    <p class="desktop-only">
                         Although not available yet, we aim to release our revolutionary record player later this year,
                         along with the accompanying app.
                     </p>
-
                 </div>
             </div>
             <div class="row part">
-                <div class="col-md-6">
+                <div class="col-md-6 col-xs-8">
                     <h3>Any questions?</h3>
                     <p>We're ready for your questions! Hit us up:
                 </div>
-                <div class="col-md-6 contact">
+                <div class="col-md-6 col-xs-4 contact">
                     <div class="row">
-                        <div class="col-md-2"><a
+                        <div class="col-md-2 col-xs-6"><a
                                     href="mailto:tim.vandijck.1@student.kdg.be, ino.vanwinckel@student.kdg.be"><i
                                         class="fa fa-envelope-o" aria-hidden="true"></i></a></div>
-                        <div class="col-md-2"><a href="https://www.facebook.com/messages/InoVW"><i
+                        <div class="col-md-2 col-xs-6"><a href="https://www.facebook.com/messages/InoVW"><i
                                         class="fa fa-facebook" aria-hidden="true"></i></a></p></div>
-                        <div class="col-md-2"></div>
-
-
                     </div>
                 </div>
             </div>
